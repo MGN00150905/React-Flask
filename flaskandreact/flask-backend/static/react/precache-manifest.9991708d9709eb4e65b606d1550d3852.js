dd18b1b15e53421f31e92b3f7a0937b8c1be619d
Marc <marcgallagher16@gmail.com>
2019-01-30 14:07:09 +0000

    flask-react-app

---
+++ flaskandreact/flask-backend/static/react/precache-manifest.9991708d9709eb4e65b606d1550d3852.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "static/react/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e3512b051edb8b37ee03",
+    "url": "static/react/js/runtime~main.e3512b05.js"
+  },
+  {
+    "revision": "8de68dc39016960900a4",
+    "url": "static/react/js/main.8de68dc3.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "static/react/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "8de68dc39016960900a4",
+    "url": "static/react/css/main.cfef5101.chunk.css"
+  },
+  {
+    "revision": "f55f81bb0435f5d0f9fd62bd0573e0c1",
+    "url": "static/react/../../../../templates/index.html"
+  }
+];
